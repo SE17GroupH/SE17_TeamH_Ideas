4c87d93f1d89409a925227b77bae4b93bccf962f
Harold Chen <ychen71@ncsu.edu>
2017-01-22 00:25:30 -0500

    Update latest code, working on recording failed login times tomorrow

--- userLoginExp/app/controllers/users_controller.rb
+++ userLoginExp/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def show
   	@user = User.find(params[:id])
   	# debugger
@@ -11,9 +12,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    	flash[:success] = "Welcome to the User Login Experiment!"
-    	redirect_to @user
-      # Handle a successful save.
+      log_in @user
+      flash[:success] = "Welcome to the User Login Experiment!"
+      redirect_to @user
     else
       render 'new'
     end
--- userLoginExp/db/schema.rb
+++ userLoginExp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121212622) do
+ActiveRecord::Schema.define(version: 20170122050259) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 20170121212622) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.datetime "open_page_at"
+    t.datetime "login_at"
+    t.integer  "time_difference"
+    t.integer  "login_failed"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
