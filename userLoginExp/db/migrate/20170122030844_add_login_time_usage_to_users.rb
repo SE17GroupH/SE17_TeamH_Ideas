a04206da728ff3fecb27ca9062c5429ca74b7248
Harold Chen <ychen71@ncsu.edu>
2017-01-22 14:20:55 -0500

    Update latest code, deployed on Heroku

--- userLoginExp/db/migrate/20170122030844_add_login_time_usage_to_users.rb
+++ userLoginExp/db/migrate/20170122030844_add_login_time_usage_to_users.rb
@@ -1,6 +1,6 @@
 class AddLoginTimeUsageToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :open_page_at, :timestamp
-    add_column :users, :login_at, :timestamp
+    add_column :users, :open_page_at, :integer
+    add_column :users, :login_at, :integer
   end
 end
